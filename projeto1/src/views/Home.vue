07cd568743ed7ad89896a3aac89b2539f0a63a1f
Vitinho <vitorquintal1@gmail.com>
2021-04-08 17:59:12 +0100

    aula8abril_preparação vue

---
+++ projeto1/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <div>
+      <div class="alert alert-success alert-dismissible fade show" role="alert"> With Bootstrap! <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
---
+++ projeto1/src/views/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="profile">
+    <h1>This is the profile page</h1>
+    <div>
+      <b-button v-b-toggle.collapse-1 variant="primary">Toggle Collapse</b-button>
+      <b-collapse id="collapse-1" class="mt-2">
+        <b-card>
+          <p class="card-text">Collapse contents Here</p>
+          <b-button v-b-toggle.collapse-1-inner size="sm">Toggle Inner Collapse</b-button>
+          <b-collapse id="collapse-1-inner" class="mt-2">
+            <b-card>Hello!</b-card>
+          </b-collapse>
+        </b-card>
+      </b-collapse>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  components: {},
+};
+</script>
