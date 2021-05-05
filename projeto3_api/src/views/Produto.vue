515b11e6904265fb139da0dbcb8bd942eb640a54
gorete <goretefreitasle@gmail.com>
2021-05-05 17:58:10 +0100

    aula 5maio_acin

--- projeto3_api/src/views/Produto.vue
+++ projeto3_api/src/views/Produto.vue
@@ -9,14 +9,14 @@
                 <div class="polaroid">
                   <v-img :src="imagemL" alt="1_img" style="width:500px; height:400px;"></v-img>
                     <div class="container">
-                      <p>{{factos[0]}}</p>
+                      <p>{{factos[5]}}</p>
                     </div>
                 </div>
 
                 <div class="polaroid">
                   <v-img :src="imagemR" alt="2_img" style="width:500px; height:400px;"></v-img>
                     <div class="container">
-                      <p>{{factos[2]}}</p>
+                      <p>{{factos[7]}}</p>
                     </div>
                 </div>
               </v-row>
@@ -88,7 +88,8 @@ export default {
       imagemR: null,
       factos: ["The oldest known dog bones were found in Asia and date as far back as 10,000 B.C. The first identifiable dog breed appeared about 9000 B.C. and was probably a type of Greyhound dog used for hunting.",
       "There are an estimated 400 million dogs in the world.", 
-      "It is much easier for dogs to learn spoken commands if they are given in conjunction with hand signals or gestures."],
+      "It is much easier for dogs to learn spoken commands if they are given in conjunction with hand signals or gestures.","Over half of dog owners include their dogs in annual holiday photos.","A dog's average body temperature is 101.2 degrees.","Some studies believe that dogs started to be domesticated 33,000 years ago.","Dogs belong to a biological family called the Canidae, a member of this family is called a canid. This is the origin of the adjective \"canine\" which means \"of or like a dog, relating to or characteristic of dogs\".","Dogs have a very good sense of smell. The part of a dog\u2019s brain that analyses smell is 40 times larger than a human\u2019s and they can smell 1,000 to 10,000 times better than us.","About one-third of a dog\u2019s brain is dedicated to smell.","The breed of dog with the best sense of smell is the bloodhound.","Dog noses are also very cute and easy to boop.","A dog could detect a teaspoon of sugar if you added it to an Olympic-sized swimming pool full of water.","Dogs can be trained to detect cancer in humans."
+    ],
       rules: [
         value => (value && value.length >= 3) || 'Min 3 characters',
       ],
